315cd22eead27117bf6524cb69764d35a97bcfc0
Nahid Estes <44173168+NahidEstes@users.noreply.github.com>
2023-04-18 18:09:32 +0600

    uninstall tailwind and install bootstrap for the project purposes

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import Header from "../pages/Home/Home/Shared/Header";
+import Footer from "../pages/Home/Home/Shared/Footer";
+import { Col, Container, Row } from "react-bootstrap";
 
 const Main = () => {
-  return <div>Main</div>;
+  return (
+    <div>
+      <Header />
+      <Container>
+        <Row>
+          <Col sm={8}>sm=8</Col>
+          <Col sm={4}>sm=4</Col>
+        </Row>
+        <Row>
+          <Col sm>sm=true</Col>
+          <Col sm>sm=true</Col>
+          <Col sm>sm=true</Col>
+        </Row>
+      </Container>
+      <Footer />
+    </div>
+  );
 };
 
 export default Main;
---
+++ src/pages/Home/Home/Shared/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <p className="text-center">
+        <small>copyright of @2023</small>
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
